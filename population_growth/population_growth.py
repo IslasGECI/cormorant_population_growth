7968e387d5182a524ccd0112c5bfc5f2ab08d1bd
Andrea <a348644@uabc.edu.mx>
2020-12-02 13:23:18 -0800

    💩 💩 💩 🍻 Pre - define método de clase Tendencia_Cormoran_Pacifico

--- population_growth/population_growth.py
+++ population_growth/population_growth.py
@@ -1,4 +1,39 @@
+class Tendencia_Cormoran_Pacifico:
 
-class Population_trend():
-    def f(self):
-        return "Hello World"
+    def set_isle(self):
+        plot_data = cormorant_data[cormorant_data.Isla == islet]
+        season = plot_data["Intervalo"].str.split("-", n=1, expand=True)
+        plot_data["Temporada"] = season[0].astype("int32")
+        plot_data = plot_data.rename(columns={"Nidos_activos_por_visita": "Maxima_cantidad_nidos"})
+        plot_data = plot_data.reset_index(drop=True)
+
+        ticks_text = plot_data.Intervalo.unique()
+        ticks_positions = ticks_positions_array(ticks_text)
+
+    def calculate_trends(self):
+        plot_data = plot_data.dropna(subset=["Maxima_cantidad_nidos"])
+        min_value = plot_data["Maxima_cantidad_nidos"].min()
+        max_value = plot_data["Maxima_cantidad_nidos"].max()
+        time_fit = np.array(plot_data.index) + 1
+        time_to_model = np.linspace(time_fit.min(), time_fit.max(), 100)
+
+        parameters = lambda_calculator(plot_data["Temporada"], plot_data["Maxima_cantidad_nidos"])
+        lambdas_distribution, intervals = bootstrap_from_time_series(
+            plot_data,
+            "Maxima_cantidad_nidos",
+            N=20,
+            return_distribution=True,
+            remove_outliers=True,
+        )
+
+    def plot_trend(self):
+        lambdas_distribution = pd.DataFrame({"Tasa de crecimiento": lambdas_distribution})
+        p_value_mayor, p_value_menor = calculate_p_values(lambdas_distribution)
+
+        lambda_latex = generate_latex_interval_string(intervals)
+
+        lambdas_results.append([islet, lambda_latex, p_value_mayor, p_value_menor])
+
+        model_min = power_law(time_to_model, intervals[0], parameters[1])
+        model_med = power_law(time_to_model, intervals[1], parameters[1])
+        model_max = power_law(time_to_model, intervals[2], parameters[1])
