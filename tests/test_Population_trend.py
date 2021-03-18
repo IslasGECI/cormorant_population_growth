6f8c74b8d9c0e1367c86431db9b45c50f588be96
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-18 09:59:16 -0700

    🏹👾 hunt a mutant

--- tests/test_Population_trend.py
+++ tests/test_Population_trend.py
@@ -1,10 +1,12 @@
-from population_growth import Population_trend
+from population_growth import *
 import pytest
 import pandas as pd
+from pandas._testing import assert_frame_equal
 
 
-cormorant_data = pd.read_csv("tests/data_tests/conteo_nidos_cormoran_test.csv")
-def test_method__init__():
-    Population_trend()
-
+cormorant_data = pd.DataFrame({"Isla" : ["a", "a", "b"], "Temporada" : [2020, 2021, 2020]})
+expected_data = pd.DataFrame({"Isla" : ["a", "a"], "Temporada" : [2020, 2021]})
+def test_filter_data_by_islet():
+    obtained_data = filter_data_by_islet(cormorant_data, "a")
+    assert_frame_equal(expected_data, obtained_data)
 
