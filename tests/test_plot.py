e0bc24727ab1335c643a5e523da0f48792ae44fa
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-23 10:19:02 -0700

    🎨 Add spec
    "A mock must always have a spec" Stargirl Flowers:

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -7,7 +7,7 @@ from population_growth import *
 
 @pytest.mark.mpl_image_compare
 def tests_Plotter_Population_Trend_Model():
-    model = Mock()
+    model = Mock(spec=Population_Trend_Model)
     model.time_to_model = [1, 3]
     model.model_med = [1, 3]
     Plotter = Plotter_Population_Trend_Model()
