2175f18426228f9ed60e264a21d631ce4a0f73d6
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-03-25 16:02:31 -0700

    ✅ Add hook to make scripts executable before running tests

--- spec/src_spec.sh
+++ spec/src_spec.sh
@@ -1,24 +1,29 @@
 #shellcheck shell=sh
+Describe 'scripts in src/'
+  executable() { chmod +x src/*; }
+  BeforeAll 'executable'
 
-Describe 'script calculate_cormorant_growth_rate'
-  cleanup() { rm --force tests/cormorant_all_islets_growth_rates.csv; }
-  BeforeAll 'cleanup'
-  AfterEach 'cleanup'
+  Describe 'script calculate_cormorant_growth_rate'
+    cleanup() { rm --force tests/cormorant_all_islets_growth_rates.csv; }
+    setup() { mkdir --parents reports/figures; }
+    BeforeAll 'cleanup' 'setup'
+    AfterEach 'cleanup'
 
-  It 'generates output file'
-    When call src/calculate_cormorant_growth_rate --input tests/data_tests/cormorant_all_islets_clean_data_test.csv --output tests/cormorant_all_islets_growth_rates.csv
-    The stdout should be present
-    The file tests/cormorant_all_islets_growth_rates.csv should be exist
+    It 'generates output file'
+      When call src/calculate_cormorant_growth_rate --input tests/data_tests/cormorant_all_islets_clean_data_test.csv --output tests/cormorant_all_islets_growth_rates.csv
+      The stdout should be present
+      The file tests/cormorant_all_islets_growth_rates.csv should be exist
+    End
   End
-End
 
-Describe 'script query_burrows_quantity_data'
-  cleanup() { rm --force tests/cormorant_all_islets_clean_data.csv; }
-  BeforeAll 'cleanup'
-  AfterEach 'cleanup'
+  Describe 'script query_burrows_quantity_data'
+    cleanup() { rm --force tests/cormorant_all_islets_clean_data.csv; }
+    BeforeAll 'cleanup'
+    AfterEach 'cleanup'
 
-  It 'generates output file'
-    When call src/query_burrows_quantity_data --input tests/data_tests/cormorant_all_islets_data_test.csv --output tests/cormorant_all_islets_clean_data.csv
-    The file tests/cormorant_all_islets_clean_data.csv should be exist
+    It 'generates output file'
+      When call src/query_burrows_quantity_data --input tests/data_tests/cormorant_all_islets_data_test.csv --output tests/cormorant_all_islets_clean_data.csv
+      The file tests/cormorant_all_islets_clean_data.csv should be exist
+    End
   End
 End
