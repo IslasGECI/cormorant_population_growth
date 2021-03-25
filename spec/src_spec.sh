ac4de2bea9d2221eb286ca57649233f3db0368a1
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-03-25 14:24:33 -0700

    ✅ Test query_burrows_quantity_data

--- spec/src_spec.sh
+++ spec/src_spec.sh
@@ -1,4 +1,5 @@
 #shellcheck shell=sh
+
 Describe 'script calculate_cormorant_growth_rate'
   cleanup() { rm --force tests/cormorant_all_islets_growth_rates.csv; }
   BeforeAll 'cleanup'
@@ -8,5 +9,16 @@ Describe 'script calculate_cormorant_growth_rate'
     When call src/calculate_cormorant_growth_rate --input tests/data_tests/cormorant_all_islets_clean_data_test.csv --output tests/cormorant_all_islets_growth_rates.csv
     The stdout should be present
     The file tests/cormorant_all_islets_growth_rates.csv should be exist
+  End
+End
+
+Describe 'script query_burrows_quantity_data'
+  cleanup() { rm --force tests/cormorant_all_islets_clean_data.csv; }
+  BeforeAll 'cleanup'
+  AfterEach 'cleanup'
+
+  It 'generates output file'
+    When call src/query_burrows_quantity_data --input tests/data_tests/cormorant_all_islets_data_test.csv --output tests/cormorant_all_islets_clean_data.csv
+    The file tests/cormorant_all_islets_clean_data.csv should be exist
   End
 End
