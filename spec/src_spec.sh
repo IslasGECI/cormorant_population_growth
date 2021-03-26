e7759114ca72ecd0b92e4e2e5b016da582a3f5ca
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-03-26 05:50:57 -0700

    ✅🏹👾 Add assertion to check output file

--- spec/src_spec.sh
+++ spec/src_spec.sh
@@ -8,9 +8,11 @@ Describe 'scripts in src/'
     AfterEach 'cleanup'
 
     It 'generates output file'
+      checksum() { md5sum tests/cormorant_all_islets_growth_rates.csv | cut -d " " -f1; }
       When call src/calculate_cormorant_growth_rate --input tests/data_tests/cormorant_all_islets_clean_data_test.csv --output tests/cormorant_all_islets_growth_rates.csv
       The stdout should be present
       The file tests/cormorant_all_islets_growth_rates.csv should be exist
+      The result of function checksum should eq "3bbd56343572f3708ebdb6613c11e450"
     End
   End
 
